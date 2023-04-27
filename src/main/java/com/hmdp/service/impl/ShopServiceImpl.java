4f18af23417aba74aa3382f7ca336fcc0b26066f
vkso <yuchenv@qq.com>
2023-04-27 23:53:00 +0800

    基于逻辑过期的方式，解决缓存击穿问题。

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -2,18 +2,25 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONTokener;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisData;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
@@ -38,7 +45,8 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 //        Shop shop = queryWithPassThrough(id);
 
         // 互斥锁解决缓存击穿
-        Shop shop = queryWithMutex(id);
+//        Shop shop = queryWithMutex(id);
+        Shop shop = queryWithLogicalExpire(id);
         if (null == shop) {
             return Result.fail("店铺不存在！");
         }
@@ -99,6 +107,52 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         return shop;
     }
 
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    public Shop queryWithLogicalExpire(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        // 从 redis 中查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 判断是否存在
+        if (StrUtil.isBlank(shopJson)) {
+            return null;
+        }
+        // 存在，判断是否过期, 先将 json 反序列化
+        RedisData redisData = JSONUtil.toBean(shopJson, RedisData.class);
+        Shop shop = JSONUtil.toBean((JSONObject) redisData.getData(), Shop.class);
+        LocalDateTime expireTime = redisData.getExpireTime();
+
+        // 然后判断是否过期
+        if (expireTime.isAfter(LocalDateTime.now())) {
+            // 未过期，直接返回店铺信息
+            return shop;
+        }
+
+        // 已过期，需要重建缓存
+        String lockKey = LOCK_SHOP_KEY + id;
+        boolean isLock = tryLock(lockKey);
+        if (isLock) { // 获取锁
+            // todo 开启独立线程，实现缓存重建
+            if (expireTime.isBefore(LocalDateTime.now())) {  // 再次判断 redis 中的缓存是否过期，DoubleCheck
+                // 推荐使用 线程池进行重建
+                CACHE_REBUILD_EXECUTOR.submit(() -> {
+                    try {
+                        this.saveShop2Redis(id, 30L);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        // 释放锁
+                        unlock(lockKey);
+                    }
+                });
+
+            }
+        }
+
+        // 获取锁失败，返回过期店铺信息
+        return shop;
+    }
+
     public Shop queryWithPassThrough(Long id) throws InterruptedException {
         // 1. 从 Redis 查询商铺缓存
         String key = CACHE_SHOP_KEY + id;
@@ -138,6 +192,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     private void unlock(String key) {
         stringRedisTemplate.delete(key);
+    }
+
+    public void saveShop2Redis(Long id, Long expireSeconds) throws InterruptedException {
+        // 查询店铺数据
+        Shop shop = getById(id);
+        Thread.sleep(200);
+
+        // 封装逻辑过期时间
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expireSeconds));
+
+        // 写入 Redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(redisData));
     }
 
     @Override
